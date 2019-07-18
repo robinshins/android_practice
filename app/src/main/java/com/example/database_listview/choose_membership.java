78fe9668bf4502e0a5311330c70bfd01c30bf7af
Administrator <robinshins674@gmail.com>
2019-07-18 18:17:25 +0900

    add time limit, back conditioning
    문자인증 시간제한 기능 추가 및 뒤로가기 키 핸들링( 기본정보 입력 화면 이후에서 뒤로가기 버튼 누르면 의사 확인후 메인화면으로 돌아가며 추가되었던 데이터도 데이터베이스에서 삭제 됨)

--- app/src/main/java/com/example/database_listview/choose_membership.java
+++ app/src/main/java/com/example/database_listview/choose_membership.java
@@ -1,7 +1,9 @@
 package com.example.database_listview;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,6 +22,7 @@ public class choose_membership extends AppCompatActivity {
     int price2 = 130000;
     int price3= 240000;
     int price4 = 430000;
+    MySQliteHandler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +32,7 @@ public class choose_membership extends AppCompatActivity {
         SimpleDateFormat format1 = new SimpleDateFormat("yyyyMMdd");
         Date time = new Date();
         currentdate= format1.format(time); // 현재 날짜
-        final MySQliteHandler handler = MySQliteHandler.open(getApplicationContext());
+        handler = MySQliteHandler.open(getApplicationContext());
         ListView listView;
         ListViewAdapter adapter;
         adapter = new ListViewAdapter();
@@ -60,6 +63,7 @@ public class choose_membership extends AppCompatActivity {
                     Intent intent1 = new Intent(getApplicationContext(),choose_payment.class);
                     intent1.putExtra("month",Integer.toString(mon));
                     intent1.putExtra("price",price1);
+                    intent1.putExtra("name",name);
                     startActivity(intent1);
                 }else if(month.equals("3개월권")){
                     int mon=3;
@@ -75,6 +79,7 @@ public class choose_membership extends AppCompatActivity {
                     Intent intent1 = new Intent(getApplicationContext(),choose_payment.class);
                     intent1.putExtra("month",Integer.toString(mon));
                     intent1.putExtra("price",price2);
+                    intent1.putExtra("name",name);
                     startActivity(intent1);
                 }else if(month.equals("6개월권")){
                     int mon = 6;
@@ -90,6 +95,7 @@ public class choose_membership extends AppCompatActivity {
                     Intent intent1 = new Intent(getApplicationContext(),choose_payment.class);
                     intent1.putExtra("month",Integer.toString(mon));
                     intent1.putExtra("price",price3);
+                    intent1.putExtra("name",name);
                     startActivity(intent1);
                 }else if(month.equals("12개월권")){
                     int mon=12;
@@ -105,6 +111,7 @@ public class choose_membership extends AppCompatActivity {
                     Intent intent1 = new Intent(getApplicationContext(),choose_payment.class);
                     intent1.putExtra("month",Integer.toString(mon));
                     intent1.putExtra("price",price4);
+                    intent1.putExtra("name",name);
                     startActivity(intent1);
 
                 }
@@ -123,6 +130,31 @@ public class choose_membership extends AppCompatActivity {
         return format.format(cal.getTime());
 
     }
+    public void onBackPressed() {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("가입절차를 중단하고 메인화면으로 이동하시겠습니까?");
+            builder.setPositiveButton("예",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent intent = new Intent(getApplicationContext(),start_Activity.class);
+                            startActivity(intent);
+                            handler.delete(name);  // 이과정에서 백버튼을 누르면 데이터 베이스 삭제해야 함, 중복 데이터 베이터 베이스 생성 막기
+
+                        }
+                    });
+            builder.setNegativeButton("아니오",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                        }
+                    });
+
+            builder.show();
+
+
+    }
+
+
+
 
 
 }
--- app/src/main/java/com/example/database_listview/choose_payment.java
+++ app/src/main/java/com/example/database_listview/choose_payment.java
@@ -1,7 +1,9 @@
 package com.example.database_listview;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +11,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -27,9 +30,12 @@ public class choose_payment extends AppCompatActivity {
     Button button;
     int month;
     Button button2;
+    CheckBox checkBox;
+    MySQliteHandler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        handler = MySQliteHandler.open(getApplicationContext());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_payment);
         course_name =(TextView) findViewById(R.id.course_name) ;
@@ -62,14 +68,18 @@ public class choose_payment extends AppCompatActivity {
             public void onNothingSelected(AdapterView<?> adapterView) {
             }
         });
+        checkBox = (CheckBox) findViewById(R.id.payment_checkBox);
 
         button = (Button) findViewById(R.id.start_payment);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(checkBox.isChecked()){
                 String uri = String.format( "appposw://card-approval?&authKey=1234&inputAmount=%s&inputMonth=%s&inputAmountEditable=false&runStrategy=without-input-view&inputApprovalNo=77&runMode=null&printStrategy=%s", price,month,"forced");
                 Intent intent = new Intent(Intent.ACTION_MAIN, Uri.parse(uri));
-                startActivityForResult(intent,333);
+                startActivityForResult(intent,333);}else{
+                    Toast.makeText(getApplicationContext(),"동의에 체크를 해주셔야 결제 진행이 가능합니다",Toast.LENGTH_LONG).show();
+                }
 
             }
         });
@@ -78,14 +88,42 @@ public class choose_payment extends AppCompatActivity {
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(checkBox.isChecked()){
                 String uri = String.format( "appposw://card-approval?&authKey=1234&inputAmount=%s&inputMonth=%s&inputAmountEditable=false&runStrategy=without-input-view&inputApprovalNo=77&runMode=null&printStrategy=%s", 10,month,"forced");
                 Intent intent = new Intent(Intent.ACTION_MAIN, Uri.parse(uri));
                 startActivityForResult(intent,333);
-                Log.d("할부개월",""+month);
+                Log.d("할부개월",""+month);;}else{
+                    Toast.makeText(getApplicationContext(),"동의에 체크를 해주셔야 결제 진행이 가능합니다",Toast.LENGTH_LONG).show();
+                }
 
             }
         });
 
+
+
+    }
+
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("가입절차를 중단하고 메인화면으로 이동하시겠습니까?");
+        builder.setPositiveButton("예",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(getApplicationContext(),start_Activity.class);
+                        startActivity(intent);
+                        Intent intent1 = getIntent();
+                        String name = intent1.getStringExtra("name");
+                        handler.delete(name);  // 이과정에서 백버튼을 누르면 데이터 베이스 삭제해야 함, 중복 데이터 베이터 베이스 생성 막기
+
+                    }
+                });
+        builder.setNegativeButton("아니오",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+
+        builder.show();
 
 
     }
